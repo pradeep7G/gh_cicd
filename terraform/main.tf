5b168ec9953a01bcc9705e14f5ce7c6283191a5b
pradeep7G <pradeepkumarreddy.p19@iiits.in>
2023-01-05 16:48:50 +0530

    updated workflow to comment changes in terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,18 +25,3 @@ provider "aws" {
   profile = "default"
   region  = var.region
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-  count = var.instance_count_needed ? var.instance_count : 1
-
-  user_data = <<-EOF
-  #!/bin/bash
-  echo "This script was executed from user_data"
-  EOF
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
