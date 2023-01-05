2f8e0ceebe34331ad0a5adc997cb32b1c3492b81
pradeep7G <pradeepkumarreddy.p19@iiits.in>
2023-01-05 16:55:42 +0530

    updated work flow to see changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,19 @@
-variable "region" {
-default = "us-west-2"
-}
-
-variable "instance_count_needed" {
-  default = "true"
-}
-
-variable "instance_count" {
-  default = 2
-}
-
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+   }
+ }
 }
-
+ 
 provider "aws" {
-  profile = "default"
-  region  = var.region
+ region = "us-west-2"
+}
+ 
+resource "aws_instance" "test_instance" {
+ ami           = "ami-830c94e3"
+ instance_type = "t2.nano"
+ tags = {
+   Name = "test_instance"
+ }
 }
